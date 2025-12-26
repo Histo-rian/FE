cdb5aeb07b610377f6f8a418290e211cede97203
woo-rin <dlqk091028@gmail.com>
2025-12-26 22:13:26 +0900

    sign up& Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LandingPage from './pages/Landing/LandingPage'
+import SignupPage from './pages/Signup/SignupPage'
+import LoginPage from './pages/Login/LoginPage'
 
 function App() {
-  return(
-    <BrowserRouter>	
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   )
